e7acd0838edc73d40d54082544e95ab0b88fb102
SANG PHẠM HỮU <phamhuusang@vietbando.vn>
2024-05-14 16:25:00 +0700

    add event for button

--- Expense/Expense/Expense.cpp
+++ Expense/Expense/Expense.cpp
@@ -5,13 +5,59 @@ Expense::Expense(QWidget *parent)
 {
     ui.setupUi(this);
 
-    connect(ui.login, &QPushButton::clicked, this, &Expense::on_button_clicked);
+    connect(ui.login, &QPushButton::clicked, this, &Expense::on_login_clicked);
+    connect(ui.no_account_btn, &QPushButton::clicked, this, &Expense::on_no_account_clicked);
+    connect(ui.forget_pass_btn, &QPushButton::clicked, this, &Expense::on_forget_pass_clicked);
+    connect(ui.change, &QPushButton::clicked, this, &Expense::on_change_pass_clicked);
+
+    connect(ui.about_me, &QPushButton::clicked, this, &Expense::on_about_me_clicked);
+    connect(ui.about_me_ex, &QPushButton::clicked, this, &Expense::on_about_me_clicked);
+
+    connect(ui.update_btn, &QPushButton::clicked, this, &Expense::on_update_clicked);
+    connect(ui.logout, &QPushButton::clicked, this, &Expense::on_logout_clicked);
+
+    connect(ui.register_btn, &QPushButton::clicked, this, &Expense::on_register_clicked);
 }
 
 Expense::~Expense()
 {}
 
-void Expense::on_button_clicked()
+//-------------------------------login------------------------------//
+void Expense::on_login_clicked()
 {
-    QMessageBox::information(this, "Button Clicked", "You have clicked the button!");
+    ui.tabwidget->setCurrentIndex(5);
+}
+void Expense::on_no_account_clicked()
+{
+    ui.tabwidget->setCurrentIndex(2);
+}
+void Expense::on_forget_pass_clicked()
+{
+    ui.tabwidget->setCurrentIndex(1);
+}
+//-------------------------------register------------------------------//
+void Expense::on_register_clicked()
+{
+    ui.tabwidget->setCurrentIndex(0);
+}
+//-------------------------------forget password------------------------------//
+void Expense::on_change_pass_clicked()
+{
+    ui.tabwidget->setCurrentIndex(0);
+}
+//-------------------------------update user------------------------------//
+void Expense::on_update_clicked()
+{
+    //ui.tabwidget->setCurrentIndex(0);
+    QMessageBox::information(this, "Msg", "Update successfully!");
+}
+//-------------------------------adminpage------------------------------//
+void Expense::on_logout_clicked()
+{
+    ui.tabwidget->setCurrentIndex(0);
+}
+//-------------------------------expense------------------------------//
+void Expense::on_about_me_clicked()
+{
+    ui.tabwidget->setCurrentIndex(3);
 }
--- Expense/Expense/Expense.h
+++ Expense/Expense/Expense.h
@@ -13,7 +13,21 @@ public:
     ~Expense();
 
 private slots:
-    void on_button_clicked();
+    //-------------------------------login------------------------------//
+    void on_login_clicked();
+    void on_no_account_clicked();
+    void on_forget_pass_clicked();
+    //-------------------------------register------------------------------//
+    void on_register_clicked();
+    //-------------------------------forget password------------------------------//
+    void on_change_pass_clicked();
+    //-------------------------------update user------------------------------//
+    void on_update_clicked();
+    void on_logout_clicked();
+    //-------------------------------adminpage------------------------------//
+
+    //-------------------------------expense------------------------------//
+    void on_about_me_clicked();
 
 private:
     Ui::Form ui;
